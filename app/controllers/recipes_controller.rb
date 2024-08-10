04dad6493b70812e8bf7ef5505d1f29ebc004ca0
Cassandra Calescu <casscalescu@gmail.com>
2024-08-10 21:13:45 +0200

    capture form data in controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,7 @@
 class RecipesController < ApplicationController
   def index
+    parsed_ingredients = JSON.parse(params[:ingredients])
+    ingredients_array = ingredients_array.map { |ingredient| ingredient['value'] }
   end
 
   def show
