7ee937b719a4b38b07d4bda7fdee1fc6ed3c0c30
Cassandra Calescu <casscalescu@gmail.com>
2024-08-10 20:00:58 +0200

    Populate DB with recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,29 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
 require 'json'
+require 'pry-byebug'
 
 recipes_json = Rails.root.join('recipes-en.json')
 serialized_recipes = File.read(recipes_json)
 recipes = JSON.parse(serialized_recipes)
 
-pp recipes.last
+puts 'Cleaning DB'
+Recipe.destroy_all
+
+puts 'Seeding recipes'
+
+recipes.each do |recipe|
+    new_recipe = Recipe.new
+    new_recipe.title = recipe['title']
+    new_recipe.cook_time = recipe['cook_time']
+    new_recipe.prep_time = recipe['prep_time']
+    new_recipe.ingredients = recipe['ingredients']
+    new_recipe.ratings = recipe['ratings']
+    new_recipe.cuisine = recipe['cuisine']
+    new_recipe.category = recipe['category']
+    new_recipe.author = recipe['author']
+    new_recipe.save!
+end
+
+puts 'Seeding complete'
 
