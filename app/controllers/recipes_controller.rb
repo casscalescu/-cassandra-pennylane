17bfb0a04f4131a91173ae1d0940beeda3e0c334
Cassandra Calescu <casscalescu@gmail.com>
2024-08-11 11:50:06 +0200

    Handled no ingredient input

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,19 @@
 class RecipesController < ApplicationController
   def index
-    user_ingredients = JSON.parse(params[:ingredients])
-    ingredients_array = user_ingredients.map { |ingredient| ingredient['value'] }
-    filter_recipes = Recipe.all.select do |recipe|
-      ingredients_array.all? do |ingredient|
-        recipe.ingredients.any? { |x| x.downcase.split.include?(ingredient.downcase) }
+    if params[:ingredients] != ""
+      user_ingredients = JSON.parse(params[:ingredients])
+      ingredients_array = user_ingredients.map { |ingredient| ingredient['value'] }
+      filter_recipes = Recipe.all.select do |recipe|
+        ingredients_array.all? do |ingredient|
+          recipe.ingredients.any? { |x| x.downcase.split.include?(ingredient.downcase) }
+        end
       end
+      @recipes = filter_recipes.sort_by { |recipe| recipe.ingredients.count - ingredients_array.count }.first(10)
+      @message = "Your top results..."
+    else
+      @recipes = Recipe.order('RANDOM()').limit(10)
+      @message = "Oops you didn't tell us what ingredients you have... here's some recipes you may like"
     end
-    @recipes = filter_recipes.sort_by { |recipe| recipe.ingredients.count - ingredients_array.count }.first(10)
   end
 
   def show
