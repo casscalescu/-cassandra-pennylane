b6c1927f4c4448422dbd355ddca6c210de5733a5
Cassandra Calescu <casscalescu@gmail.com>
2024-08-14 19:47:03 +0300

    Logic for when only 1 recipe result - display recipes containing at least 1 ingredient'

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,10 +5,22 @@ class RecipesController < ApplicationController
       ingredients_array = user_ingredients.map { |ingredient| ingredient['value'] }
       filter_recipes = Recipe.all.select do |recipe|
         ingredients_array.all? do |ingredient|
+          # split to look at each word => avoid "apple" matching "pineapple" in a String
           recipe.ingredients.any? { |x| x.downcase.split.include?(ingredient.downcase) }
         end
       end
-      @recipes = filter_recipes.sort_by { |recipe| recipe.ingredients.count - ingredients_array.count }.first(10)
+      if filter_recipes.count > 1
+        @recipes = filter_recipes.sort_by { |recipe| recipe.ingredients.count - ingredients_array.count }.first(10)
+      else
+        suggested_recipes = Recipe.all.select do |recipe|
+          ingredients_array.any? do |user_ingredient|
+            recipe.ingredients.any? do |recipe_ingredient|
+              recipe_ingredient.split.include?(user_ingredient.downcase)
+            end
+          end
+        end
+        @recipes = suggested_recipes.shuffle.first(10)
+      end
       @message = "Your top results..."
     else
       @recipes = Recipe.order('RANDOM()').limit(10)
