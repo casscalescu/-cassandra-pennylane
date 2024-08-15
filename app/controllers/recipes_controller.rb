bc0adb027b9650456f3490053cb0427faff6175d
Cassandra Calescu <casscalescu@gmail.com>
2024-08-15 17:22:22 +0300

    Move logic from controller to model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,16 +3,16 @@ class RecipesController < ApplicationController
     if params[:ingredients].present?
       user_ingredients = JSON.parse(params[:ingredients])
       ingredients_array = user_ingredients.map { |ingredient| ingredient['value'] }
-      recipe_results = recipes_exact_match(ingredients_array)
+      recipe_results = Recipe.recipes_exact_match(ingredients_array)
       @recipes = recipe_results.sort_by { |recipe| recipe.ingredients.count - ingredients_array.count }.first(10)
       @message = "Your top results..."
       if recipe_results.count < 10
-        suggested_recipes = recipes_conditional_match(ingredients_array)
+        suggested_recipes = Recipe.recipes_conditional_match(ingredients_array)
         @additional_recipes = suggested_recipes.shuffle.first(10)
-        @additional_message = "Some additional recipes containing some of your ingredients for some extra inspiration..."
-      end 
+        @additional_message = "Since your results are so short, here's some additional recipes to inspire you..."
+      end
     else
-      @recipes = recipes_random
+      @recipes = Recipe.random_recipes
       @message = "Oops you didn't tell us what ingredients you have... here's some recipes you might like"
     end
   end
@@ -25,30 +25,5 @@ class RecipesController < ApplicationController
 
   def set_recipe
     @recipe = Recipe.find(params[:id])
-  end
-
-  def recipes_exact_match(ingredients_array)
-    ingredients_array = ingredients_array.map(&:downcase) # Normalize the ingredient names
-
-    Recipe.all.select do |recipe|
-      ingredients_array.all? do |ingredient|
-        # split to look at each word => avoid "apple" matching "pineapple" in a String
-        recipe.ingredients.any? { |x| x.downcase.split.include?(ingredient.downcase) }
-      end
-    end
-  end
-
-  def recipes_conditional_match(ingredients_array)
-    Recipe.all.select do |recipe|
-      ingredients_array.any? do |user_ingredient|
-        recipe.ingredients.any? do |recipe_ingredient|
-          recipe_ingredient.split.include?(user_ingredient.downcase)
-        end
-      end
-    end
-  end
-
-  def recipes_random
-    Recipe.order('RANDOM()').limit(10)
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,27 @@
 class Recipe < ApplicationRecord
+    def self.recipes_exact_match(ingredients_array)
+        ingredients_array.map(&:downcase)
+
+        Recipe.all.select do |recipe|
+            ingredients_array.all? do |ingredient|
+                recipe.ingredients.any? { |x| x.downcase.split.include?(ingredient) }
+            end
+        end
+    end
+
+    def self.recipes_conditional_match(ingredients_array)
+        ingredients_array.map(&:downcase)
+
+        Recipe.all.select do |recipe|
+          ingredients_array.any? do |user_ingredient|
+            recipe.ingredients.any? do |recipe_ingredient|
+              recipe_ingredient.split.include?(user_ingredient)
+            end
+          end
+        end
+      end
+
+    def self.random_recipes
+        order('RANDOM()').limit(10)
+    end
 end
